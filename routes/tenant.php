078884b9a22e5a7c2fec41cdc88a7dac9a632e88
petrospe <petros_pe@yahoo.gr>
2025-08-08 19:16:18 +0300

    fix profile update

--- routes/tenant.php
+++ routes/tenant.php
@@ -39,6 +39,8 @@ Route::middleware([
     // Dashboard (Inertia/Vue)
     Route::middleware(['auth'])->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
         Route::get('/dashboard', function () {
             return inertia('Dashboard');
